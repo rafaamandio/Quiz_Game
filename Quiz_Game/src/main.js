1f45cad86c8f1548577ddea63dba3e066bcfadfb
rafaamandio <rafael.amandio@gmail.com>
2025-01-24 11:43:24 -0300

    Deploy Script

--- Quiz_Game/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue.js'
-import axios from 'axios.js'
-import VueAxios from 'vue-axios.js'
-import './assets/style.css'
-import App from './App.vue'
-
-createApp(App)
-.use(VueAxios, axios)
-.mount('#app')
